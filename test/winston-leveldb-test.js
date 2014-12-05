db1548d00f4198fd564ae75a7ca0bd158b445c58
Brian Dentino <brian.dentino@gmail.com>
2014-12-05 13:02:43 -0800

    Cleanup

--- test/winston-leveldb-test.js
+++ test/winston-leveldb-test.js
@@ -1,24 +1,21 @@
 var winston = require('winston')
-  , levellog = require('../lib/winston-leveldb.js').LevelDB
+  , leveldb = require('../lib/winston-leveldb.js').LevelDB
+  , format = require('util').format
 
 var location = './log-test'
 var options1 = { location: './log-test', valueEncoding: 'json' }
 var options2 = { location: './log-test', valueEncoding: 'json', sublevels: ['level', 'awesome'] }
 
-winston.loggers.add('basic', {
-  LevelDB: options1
-})
-winston.loggers.get('basic').info("I'm logging into leveldb!", { awesome: true }, 
+var basic = winston.loggers.add('basic')
+basic.add(leveldb, options1);
+basic.info("I'm logging into leveldb!", { awesome: true }, 
   function(error) {
     console.log("Finished 1: (error=" + error + ")");
   })
 
-
-winston.loggers.add('sublevels', {
-  LevelDB: options2
-})
-
-winston.loggers.get('sublevels').info("I'm logging into leveldb sublevels", { awesome: 'indubitably' },
+var sublevels = winston.loggers.add('sublevels')
+sublevels.add(leveldb, options2);
+sublevels.info("I'm logging into leveldb sublevels", { awesome: 'indubitably' },
   function(error) {
     console.log("Finished 2: (error=" + error + ")")
   })
